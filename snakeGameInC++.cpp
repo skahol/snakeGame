b7d46e26ddbd1feb421fdb38bc6208a0dc57e6cc
skahol <skahol2000@gmail.com>
2019-03-10 23:42:00 +0530

    transmitted

--- snakeGameInC++.cpp
+++ snakeGameInC++.cpp
@@ -2,10 +2,13 @@
 #include<time.h>
 #include<queue>
 #include<vector>
+#include<algorithm>
+#include<conio.h>		// for kbhit()
 using namespace std;
-#define n 7
-#define m 7
+#define n 10
+#define m 14
 /* adding fruit  */
+static int score;
 class X
 {
 	public:
@@ -26,20 +29,20 @@ queue<X> q;
 
 void snakeGen(char a[n][m])		// call by reference
 {
-	// class object to get current position of head (row,col)
+// class object to get current position of head (row,col)
 	X x1;		//---> to get head's current position by updating same object.
-	 // This program will create different sequence of  
-    // random numbers on every program run  
+// This program will create different sequence of  
+// random numbers on every program run  
   
-    // Use current time as seed for random generator 
+// Use current time as seed for random generator 
     srand(time(0));
     
 	int hr, hc;
 	int tr, tc;
-	// for head coord in grid
+// for head coord in grid
 	hr=rand()%n;	
 	hc=rand()%m;
-	// for tail coord in grid
+// for tail coord in grid
 	tr=rand()%3+(hr-1);
 	if(tr<0)		// case if head is at corner.
 		tr=-tr;
@@ -58,11 +61,11 @@ void snakeGen(char a[n][m])		// call by reference
 	else {					// --> if(tr<tc || tr>tc) tc=hc;
 		tc=hc;		
 	}
-	// head is represented by 0
-	// body  & tail by 1
+// head is represented by 0
+// body  & tail by 1
 	a[hr][hc]='0';
 	a[tr][tc]='1';
-	// initial position of snake  in queue 
+// initial position of snake  in queue 
 	x1.r=tr;
 	x1.c=tc;
 	q.push(x1);
@@ -77,37 +80,47 @@ void fruit(char a[n][m])
 	int fc;
 	fr=rand()%n;			
 	fc=rand()%m;
-	//--> vector for dynamic allocation
+//--> vector for dynamic allocation
 	vector<int> vrow;
 	vector<int> vcol;		
-	// vrow having rows of snake and same for vcol
-	//-->rows and cols other than snake vr,vc.
+// vrow having rows of snake and same for vcol
+//-->rows and cols other than snake vr,vc.
 	vector<int> vr;		
 	vector<int> vc;
 	
 	queue<X> temp=q;
-	while(temp.empty()){
-		X x=temp.front();
+//--->object to access temp queue each element .
+	X x;
+	while(!temp.empty()){
+		x=temp.front();
 		vrow.push_back(x.r);
 		vcol.push_back(x.c);
 		temp.pop();
 	}
+	sort(vrow.begin(),vrow.end());
+	sort(vcol.begin(),vcol.end());
+	int k=0;
 	for(int i=0,j=0;i<n ;i++)
 	{
-		if(j<vrow.size() && vrow[j]==i)
+		if(j<vrow.size() && vrow[j]==k){
 			j++;
-		else{
+		}
+		else{					// donot push same value multiple times bcz of using i-- in if condition
 			vr.push_back(i);
 		}
 	}
+	k=0;
 	for(int i=0,j=0; i<m; i++)
 	{
-		if(j<vcol.size() && vcol[j]==i)
+		if(j<vcol.size() && vcol[j]==k){
 			j++;
+		}
 		else{
+			k++;
 			vc.push_back(i);
 		}
 	}
+	
 	for(int i=0;i<vrow.size();i++)		//---> size of vrow and vcol will same, storing coordinates of snake.
 	{
 		int s=vr.size();
@@ -115,25 +128,46 @@ void fruit(char a[n][m])
 		while(vrow[i]==fr && vcol[i]==fc){
 			fr=vr[rand()%s];
 			fc=vc[rand()%s1];
+			if(vrow.size()==n && vcol.size()==m)
+			{
+				fr=rand()%n;
+				fc=rand()%m;
+				break;
+			}
 		}
 	}	
 	a[fr][fc]='@';
+}
+void delay(double sec){
+	double millisec=1000*sec;
+	clock_t start=clock();
+	while(clock()<(start+millisec));
 }
 
 /*******************************
 return 1 game in progress
 return -1 game over
 *******************************/
-
-int  movement(char a[n][m], char move)
+bool haveFruit(char a[][m]){
+	for(int i=0;i<n;i++){
+		for(int j=0;j<m;j++){
+			if(a[i][j]=='@')
+				return true;
+		}
+	}
+	return false;
+}
+int  movement(char a[n][m],char move)
 {
-	bool isFruit = false;		
-	// obj x2 to move head to new pos.
-	// x2 other feature to clean traces of tail left after single move (set to ' ').
-	X x2=q.back();		//----> x2 object have coord of head now
-	//--->up
-	if( move=='w' && x2.r -1>=0 && a[x2.r-1][x2.c]!='-' &&a[x2.r-1][x2.c]!='|' &&a[x2.r-1][x2.c]!='1')
+	bool isFruit = false;
+					        	//---> obj x2 to move head to new pos.
+	X x2=q.back();				//---> x2 other feature to clean traces of tail left after single move (set to ' ').
+				   				//----> x2 object have coord of head now
+	if( move=='w' && x2.r -1>=0  && a[x2.r-1][x2.c]!='1') //--->up
 		{
+			if(a[x2.r-1][x2.c]=='=' || a[x2.r-1][x2.c]=='|')
+				return -1;
+			
 			if(a[x2.r-1][x2.c]=='@')
 				isFruit=true;
 				
@@ -141,9 +175,11 @@ int  movement(char a[n][m], char move)
 			a[x2.r][x2.c]='1';		//---->tail
 			q.push(X(x2.r-1,x2.c));
 		}
-	//--->down	
-	else if(move=='s' && x2.r +1<n && a[x2.r+1][x2.c]!='-' &&a[x2.r+1][x2.c]!='|' &&a[x2.r+1][x2.c]!='1')
+	else if(move=='s' && x2.r +1<n &&a[x2.r+1][x2.c]!='1') //--->down	
 		{
+			if(a[x2.r+1][x2.c]=='=' || a[x2.r+1][x2.c]=='|')
+				return -1;
+				
 			if(a[x2.r+1][x2.c]=='@')
 				isFruit=true;
 				
@@ -151,9 +187,11 @@ int  movement(char a[n][m], char move)
 			a[x2.r][x2.c]='1';
 			q.push(X(x2.r+1,x2.c));
 		}
-	//--->left
-	else if(move=='a' && x2.c-1>=0 && a[x2.r][x2.c-1]!='-' &&a[x2.r][x2.c-1]!='|' &&a[x2.r][x2.c-1]!='1')
+	else if(move=='a' && x2.c-1>=0 &&a[x2.r][x2.c-1]!='1') //--->left
 		{
+			if(a[x2.r][x2.c-1]=='=' || a[x2.r][x2.c-1]=='|')
+				return -1;
+				
 			if(a[x2.r][x2.c-1]=='@')
 				isFruit=true;
 			
@@ -161,9 +199,11 @@ int  movement(char a[n][m], char move)
 			a[x2.r][x2.c]='1';
 			q.push(X(x2.r,x2.c-1));
 		}
-	//--->right
-	else if(move=='d' && x2.c+1<m && a[x2.r][x2.c+1]!='-' &&a[x2.r][x2.c+1]!='|' &&a[x2.r][x2.c+1]!='1')
+	else if(move=='d' && x2.c+1<m && a[x2.r][x2.c+1]!='1')   //--->right
 		{
+			if(a[x2.r][x2.c+1]=='=' || a[x2.r][x2.c+1]=='|')
+				return -1;
+				
 			if(a[x2.r][x2.c+1]=='@')
 				isFruit=true;
 				
@@ -171,68 +211,94 @@ int  movement(char a[n][m], char move)
 			a[x2.r][x2.c]='1';
 			q.push(X(x2.r,x2.c+1));
 		}
-	//--->rule's voilation
-	else{
+	else if(move!='a'&& move!='s'&& move!='d'&& move!='w'){		//--->invalid move
+			return 1;
+	}
+	else if(move=='q'){				//---> quiting game
+		return 0;		
+	}
+	else{							//--->rule's voilation
 		return -1;
 	}
 	
-	if(isFruit==false){
-
-		//-->clearing tail previous pos.
+	if(!isFruit){					//-->clearing tail previous pos.
 		x2=q.front();
 		a[x2.r][x2.c]=' ';
 		q.pop();
 	}
 	else{
+		score+=10;
 		fruit(a);
 	}
+	
 	return 1;
 }
 
 void  grid(char a[n][m])
 {
-	int returnValue;
-	
-	do{
 	system("cls");
-	char move;		//--> move w, a, s, d.
-	cout<<"---------"<<endl;
+	cout<<"\tSCORE :"<<score<<endl;
+	cout<<"\t================"<<endl;
 	for( int i=0;i<n;i++)
-	{	cout<<"|";
+	{	cout<<"\t|";
 		for(int  j=0;j<m;j++)
 		{
 			cout<<a[i][j];
 		}	
 		cout<<"|"<<endl;
 	}
-	cout<<"---------"<<endl;
-	cout<<endl<<"************************************"<<endl;
-	cout<<"Press:\nw for up\na for left\ns for down\nd for right\nq for quit\n"<<endl<<"Press move: ";
-	cin>>move;
-	if(move=='q'){
-		cout<<"Game Quit"<<endl;
-		system("pause");
-		return ;		
-	}
-	returnValue = movement( a, move);
-	}while(returnValue==1);				// ---> true until game in progress
+	cout<<"\t================"<<endl;
 	
-	// return -1 then,
-		cout<<"Game Over"<<endl;
-		system("pause");
-		return ;
+	if( !haveFruit(a)){		// due to bug 
+		fruit(a);
+	}
 }
 
 int main()
 {
-	char arr[][m]={ {' ',' ',' ',' ',' ',' ',' '},
-				{' ',' ',' ',' ',' ',' ',' '},
-				{' ',' ',' ',' ',' ',' ',' '},
-				{' ',' ',' ',' ',' ',' ',' '},
-				{' ',' ',' ',' ',' ',' ',' '},
-				{' ',' ',' ',' ',' ',' ',' '},
-				{' ',' ',' ',' ',' ',' ',' '}  };
+	score=0;
+	char arr[][m]={ {' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' '},
+					{' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' '},
+					{' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' '},
+					{' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' '},
+					{' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' '},
+					{' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' '},
+					{' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' '},
+					{' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' '},
+					{' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' '},
+					{' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' '} };
 	snakeGen(arr);
 	fruit(arr);
-	grid(arr);
+	
+	int returnValue=1;
+	char move;
+	while(1){
+		
+		if(kbhit()){
+			move=getch();
+			returnValue = movement( arr, move);
+			goto label;
+		}		
+		if(move=='a' || move=='s' || move=='d' || move=='w')
+			returnValue=movement(arr,move);	
+	
+	label:
+	if(score==100){
+		cout<<"\tCongractulations!!! Level Completed"<<endl;
+		break;
+	}
+	if(returnValue==0){
+		cout<<"\tGame Quit"<<endl;
+		break;
+	}
+	
+	if(returnValue==-1){
+		cout<<"\tGame Over"<<endl;
+		break;
+	}
+		grid(arr);
+		delay(1);
+	
+	}
+
 }
